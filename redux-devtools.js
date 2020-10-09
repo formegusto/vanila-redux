11033fbf180656db2fd0f30e26238d8510df4de5
formegusto <formegusto@gmail.com>
2020-10-09 22:13:44 +0900

    일반에서도 사용가능한 redux

---
+++ redux-devtools.js
@@ -0,0 +1,18 @@
+var compose = Redux.compose;
+
+__esModule = true;
+composeWithDevTools = (
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ :
+    function() {
+      if (arguments.length === 0) return undefined;
+      if (typeof arguments[0] === 'object') return compose;
+      return compose.apply(null, arguments);
+    }
+);
+
+devToolsEnhancer = (
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION__ :
+    function() { return function(noop) { return noop; } }
+);
